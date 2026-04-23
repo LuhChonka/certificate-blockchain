63bc18ccd82f853bd1459b179eb7c59ec79d1648
LuhChonka <shyamak1102@gmail.com>
2026-04-23 15:05:40 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://shyamak:shyamak@ac-xtipslo-shard-00-00.titlxqb.mongodb.net:27017,ac-xtipslo-shard-00-01.titlxqb.mongodb.net:27017,ac-xtipslo-shard-00-02.titlxqb.mongodb.net:27017/certificates?ssl=true&replicaSet=atlas-ekyes5-shard-0&authSource=admin&appName=Cluster0");
+mongoose.connect(process.env.MONGO_URI);
 
 mongoose.connection.on("connected", () => {
   console.log("MongoDB connected");
